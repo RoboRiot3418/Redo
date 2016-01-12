b7399d4d25f95bd2f9199b591e1a49d705d1f4a8
Important Yogurt <mrska552@gmail.com>
2016-01-11 19:28:12 -0600

    hey hey hey dont break break break

--- src/org/usfirst/frc/team3418/robot/Robot.java
+++ src/org/usfirst/frc/team3418/robot/Robot.java
@@ -1,7 +1,15 @@
-
+/** 
+Eclipse Mars can go get bent.
+Eclipse Mars can go get bent.
+Eclipse Mars can go get bent.
+Eclipse Mars can go get bent.
+*/
 package org.usfirst.frc.team3418.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import java.lang.Math;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,6 +23,26 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+	 
+	 Talon motor8 = new Talon (4);
+	 
+	 Joystick stick = new Joystick (5);
+	 /** the number is the usb port
+	 */
+	 
+	 double a = 0;
+	 double deadzone = .2;
+	 
+	 /**public double deadzone(double original){
+		 if (original>(deadzone*-1) && original <deadzone){
+			return 0
+		 }
+		 */
+		 return original
+	 }
+	 
+	 
+	 
     public void robotInit() {
 
     }
@@ -29,8 +57,18 @@ public class Robot extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
-        
+    public void teleopPeriodic() 
+		
+		a=stick.getRawAxis(1);
+/* 
+pushing forward is -1
+*/
+		
+        //if (stick.getRawButton(4))
+			
+		motor8.set(a);
+		
+		
     }
     
     /**
